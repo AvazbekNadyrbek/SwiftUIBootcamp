7131ea99b036d5b7a492b41190873e76448014f4
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-04-23 11:34:00 +0200

    Initial Commit

---
+++ SwiftUIBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIBootcamp
+//
+//  Created by Авазбек Надырбек уулу on 23.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Авазбек Надырбек уулу on 23.04.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
