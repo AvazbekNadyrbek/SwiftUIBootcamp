4793617520a2cbe4230178cd3307761dda8e9904
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-04-25 17:09:00 +0200

    Learned what a Spacer is and how we can build with this better UI

---
+++ SwiftUIBootcamp/Stacks/StackChallenge/StackChallenge.swift
@@ -0,0 +1,18 @@
+//
+//  StackChallenge.swift
+//  SwiftUIBootcamp
+//
+//  Created by Авазбек Надырбек уулу on 25.04.25.
+//
+
+import SwiftUI
+
+struct StackChallenge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StackChallenge()
+}
