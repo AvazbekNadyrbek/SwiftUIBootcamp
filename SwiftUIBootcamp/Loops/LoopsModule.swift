81f86c360b3377fcbe381c115b38a02733f178aa
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-04-26 20:52:33 +0200

    Learned what a Loops and how we can use in SwiftUi

---
+++ SwiftUIBootcamp/Loops/LoopsModule.swift
@@ -0,0 +1,18 @@
+//
+//  LoopsModule.swift
+//  SwiftUIBootcamp
+//
+//  Created by Авазбек Надырбек уулу on 25.04.25.
+//
+
+import SwiftUI
+
+struct LoopsModule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoopsModule()
+}
