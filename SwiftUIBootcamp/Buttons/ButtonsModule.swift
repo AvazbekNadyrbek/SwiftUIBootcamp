a08f95a6f7d88497841876377ce6ec52e550f8d0
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-04-27 21:47:34 +0200

    Jusr learned what a List is, and what a Section and how we can customize it! I want to tell my self, that i am learning this all stuf for my self! And for creating amazing things for people, i dont wanna give up we can do it! For my future my self i hope with God i can do it!

---
+++ SwiftUIBootcamp/Buttons/ButtonsModule.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonsModule.swift
+//  SwiftUIBootcamp
+//
+//  Created by Авазбек Надырбек уулу on 27.04.25.
+//
+
+import SwiftUI
+
+struct ButtonsModule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonsModule()
+}
